7250aa2a6ae7feb8c0d046ecb2efdb9b08cb6734
shiorioxy <shiorioxy@qq.com>
2014-09-19 09:57:14 +0800

    all

---
+++ js/SubBanner.js
@@ -0,0 +1,15 @@
+﻿document.write("<div id=\"Top\" style=\"width:100%;height:120px;line-height:120px;text-align:right;\">");
+document.write("<div id=\"Logo\" style=\"width:40%;height:120px;float:left;text-align:center;\">");
+document.write("<div style=\"width:100%;height:50px;line-height:50px;font-size:40px;margin-top:30px;\">Xin-Yu Ou（欧新宇）</div>");
+document.write("<div style=\"width:100%;height:30px;line-height:30px;font-size:14px;color:gray;text-align:left;\">教育的根是苦的，但是其果实是甜的。</div>");
+document.write("</div>");
+document.write("<div id=\"BadgeHUST\" style=\"height:120px;width:140px;line-height:120px;text-align:center;float:right;\">");
+document.write("<a href=\"http://www.hust.edu.cn\" target=\"_blank\"><img src=\"../Images/hust.png\" title=\"华中科技大学\" height=\"120px\"></a>");
+document.write("</div>");
+document.write("<div id=\"BadgeYNU\" style=\"height:120px;width:140px;line-height:120px;text-align:center;float:right;\">");
+document.write("<a href=\"http://www.ynu.edu.cn\" target=\"_blank\"><img src=\"../Images/ynu.png\" title=\"云南大学\" height=\"120px\"></a>");
+document.write("</div>");
+document.write("<div id=\"BadgeYNOU\" style=\"height:120px;width:140px;line-height:120px;text-align:center;float:right;\">");
+document.write("<a href=\"http://www.ynou.cn\" target=\"_blank\"><img src=\"../Images/ynou.png\" title=\"云南开放大学\" height=\"120px\"></a>");
+document.write("</div>");
+document.write("</div>");
